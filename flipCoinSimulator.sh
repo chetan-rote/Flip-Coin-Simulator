551405ece2cbc29dc6378d287720c3e528449f9a
chetan-rote <rote.chetan@gmail.com>
2020-08-29 23:46:57 +0530

    Flips coin till one wins 21 times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -36,3 +36,35 @@ do
 done
 echo "Head wins $hcount times"
 echo "Tail wins $tcount times"
+
+#UC3 Flip coin till one of them wins 21 times
+hcount=0
+tcount=0
+
+while [[ ($hcount -lt 21) && ($tcount -lt 21) ]]
+do
+	randomCheck=$(( RANDOM%2 ))
+	if [[ $randomCheck == 0 ]]
+	then
+		#echo "Head"
+		((hcount++));
+	else
+		#echo "Tail"
+		((tcount++));
+	fi
+done
+
+echo "head count is: $hcount"
+echo "tail count is: $tcount"
+
+if [[ $hcount -eq 21 ]]
+then
+	echo "Head wins first"
+	echo "$hcount times Head is winner"
+elif [[ $tcount -eq 21 ]]
+then
+	echo "Tail wins first"
+	echo "$tcount times Tail is winner"
+else
+	echo "match is tie"
+fi
