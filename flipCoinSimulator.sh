8b25d8a02d8e47e66fe93713634ac59cea66495a
chetan-rote <rote.chetan@gmail.com>
2020-08-29 23:39:48 +0530

    Flips coin multiple times and prints who wins

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -8,7 +8,31 @@
 value=$((RANDOM%2))
 if [[ $value == 0 ]]
 then
-	echo "Head"
+	echo "Heads"
 else
-	echo "Tail"
+	echo "Tails"
 fi
+
+#UC2 Flip Coin multiple times
+count=0;
+hcount=0
+tcount=0;
+while [ $count -ne 21 ]
+do
+	var=$((RANDOM%2))
+	#echo $var
+
+	if [[ $var -eq 1 ]]
+	then
+		echo "Heads"
+		((hcount++))
+	else
+
+		echo "Tails"
+		((tcount++))
+
+	fi
+	((count++))
+done
+echo "Head wins $hcount times"
+echo "Tail wins $tcount times"
